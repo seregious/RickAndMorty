0d9caf658e8d27642de8ea221386a5cc505bfc11
seregious <seregious@gmail.com>
2022-06-11 09:39:01 +0300

    setup network manager

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.rickandmorty.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            NetworkManager.getCharacters
+            NetworkManager.getLocations
+            NetworkManager.getEpisodes
+        }
     }
 }
